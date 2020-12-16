ef1ec2494a019593ec176dce132361a29d45ee09
Ahsan S. Memon <ahsanmemon288@gmail.com>
2020-12-16 19:11:32 +0000

    add input.txt, output.txt, words.txt files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  ifstream fileReader;
+  string word;
+  int wordCount = 0;
+  fileReader.open("words.txt"); 
+  
+  if(!fileReader){
+    cout<<"File doesn't exist"<<endl;
+    exit(1);
+  }
+
+  cout<<"*** Reading dictionary ***"<<endl;
+  while(!fileReader.eof()){
+    getline(fileReader,word);
+    // cout<<word<<endl;
+    wordCount++;
+  }
+  cout<<"Reading Complete\n"
+
+  
+
+  fileReader.close();
+
+  cout<<"Total words: "<<wordCount<<endl;
+
 }
